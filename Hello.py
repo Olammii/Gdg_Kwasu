5bcf71624585e52d6f92d41ec9288f093c8db40d
Toheeb  Abdulsalam <59118584+Olammii@users.noreply.github.com>
2025-05-05 21:45:52 +0100

    Update and rename he.py to Hello.py

--- Hello.py
+++ Hello.py
@@ -7,12 +7,4 @@ print("What is your age?")
 Age = input()
 # Print Greeting message.
 print(f"Hello {Name.title()},you are {Age} years old.")
-print("Hello " + Name.title() + ", you are " + Age + " years old.")
-
-name = ""
-while name == "":
-    print("Enter your name:")
-    name = input()
-
-print(f"You are welcome {name}")
-
+print(f"You are welcome!")
