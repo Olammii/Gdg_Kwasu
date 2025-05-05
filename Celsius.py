977732fa41f709dbc5a3f19d320b28f57dc3f6ea
Toheeb  Abdulsalam <59118584+Olammii@users.noreply.github.com>
2025-05-05 13:59:35 +0100

    Add files via upload
    Week_1 assignment_2

---
+++ Celsius.py
@@ -0,0 +1,10 @@
+#!/usr/env/ python
+# Program that convert Celsius To Fahrenheit.
+print("Enter temperature in Celsius:")
+# Prompt user to input temperature in celsius.
+Celsius =int(input())
+# Formula to convert temperature from Celsius to Fahrenheit.
+Fahrenheit = ( Celsius * 9 / 5) + 32
+#print temperature in Fahrenheit.
+print(f"The {Celsius} temperature in celsius is equal to {Fahrenheit} in Fahrenheit.")
+print("Thank you!")
